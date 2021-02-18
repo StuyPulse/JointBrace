c63a4c6093107c46ec1bbeee818771aaa3701672
Alphois <74471676+Alphois@users.noreply.github.com>
2021-02-18 16:19:58 -0500

    Make code for remote controller

--- RemoteController/RemoteController.ino
+++ RemoteController/RemoteController.ino
@@ -1,19 +1,42 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial hc05(-1,-1);
+int upButtonPin = -1;
+int downButtonPin = -1;
+
 
 int targetTemp = 70;
 
 void setup() {
   hc05.begin(9600);
+    pinMode(upButtonPin, INPUT);
+    pinMode(downButtonPin, INPUT);
+}
+
+void changeTargetTemp() {
+/* if up is pressed, increase*/
+  bool upButtonState = digitalRead(upButtonPin);
+  bool downButtonState = digitalRead(downButtonPin);
+  if (upButtonState == LOW) {
+    targetTemp++;
+  }
+  else if (downButtonState == LOW) {
+    targetTemp--;
+  }
 }
 
 bool isButtonPressed() {
-   return true;
-  } 
+  bool upButtonState = digitalRead(upButtonPin);
+  bool downButtonState = digitalRead(downButtonPin);
+  return (upButtonState == LOW) || (downButtonState == Low));
+} 
 
 void loop() {
   if (isButtonPressed()) {
+    changeTargetTemp();
     hc05.write(targetTemp);
+    delay(1000);
   }
+  
 }
+
